62521a8721df6192ad0586f10d42769bd6fb4818
Lucas Henrique Da Silva Santos <100451037+LucasHenriqueSantosFacu@users.noreply.github.com>
2023-11-28 20:58:39 -0300

    Alteração solicitada no codigo para a prova

--- Minimo_quadrado/main.py
+++ Minimo_quadrado/main.py
@@ -1,45 +1,26 @@
-def minimos_quadrados(x_values, y_values):
+def regressao_linear_mmq(x_values, y_values):
     # Número de pontos
     n = len(x_values)
 
-    # Somatório de x, y, xy, x^2 e x^3
-    sum_x = sum(x_values)
-    sum_y = sum(y_values)
-    sum_xy = sum(xi * yi for xi, yi in zip(x_values, y_values))
-    sum_x_squared = sum(xi ** 2 for xi in x_values)
-    sum_x_cubed = sum(xi ** 3 for xi in x_values)
+    # Soma dos valores de x, y, x² e xy
+    soma_x = sum(x_values)
+    soma_y = sum(y_values)
+    soma_x2 = sum(x ** 2 for x in x_values)
+    soma_xy = sum(x * y for x, y in zip(x_values, y_values))
 
-    # Coeficientes a, b e c
-    denominator = n * sum_x_squared - sum_x ** 2
-    a = (n * sum_xy - sum_x * sum_y) / denominator
-    b = (sum_y - a * sum_x) / n
-    c = (sum_x_squared * sum_y - sum_x * sum_xy) / denominator
+    # Coeficientes da regressão linear (a e b)
+    coeficiente_a = (n * soma_xy - soma_x * soma_y) / (n * soma_x2 - soma_x ** 2)
+    coeficiente_b = (soma_y - coeficiente_a * soma_x) / n
 
-    return a, b, c
+    return coeficiente_a, coeficiente_b
 
 
-def main():
-    # Número de pontos
-    num_points = int(input("Informe o número de pontos: "))
+# Valores de exemplo para a regressão linear
+valores_x_exemplo = [-2, -1, 1, 3]
+valores_y_exemplo = [-3, 0, 2, 6]
 
-    # Listas para armazenar os valores de x e y
-    x_values = [0] * num_points
-    y_values = [0] * num_points
+# Obtém os coeficientes da regressão linear
+coeficiente_a, coeficiente_b = regressao_linear_mmq(valores_x_exemplo, valores_y_exemplo)
 
-    # Entrada dos valores de x e y
-    for i in range(num_points):
-        x_values[i] = float(input(f"Informe o valor do ponto x[{i + 1}]: "))
-        y_values[i] = float(input(f"Informe o valor do ponto y[{i + 1}]: "))
-
-    # Cálculo dos coeficientes usando o método dos mínimos quadrados
-    coefficients = minimos_quadrados(x_values, y_values)
-
-    # Saída dos coeficientes e da fórmula de y
-    print(f"Coeficiente a: {coefficients[0]}")
-    print(f"Coeficiente b: {coefficients[1]}")
-    print(f"Coeficiente c: {coefficients[2]}")
-    print(f"Fórmula de y: y = {coefficients[0]}x^2 + {coefficients[1]}x + {coefficients[2]}")
-
-
-if __name__ == "__main__":
-    main()
+# Imprime a equação da reta resultante: f(x) = ax + b
+print("Equação da Reta: f(x) = {:.4f}x + {:.4f}".format(coeficiente_a, coeficiente_b))
